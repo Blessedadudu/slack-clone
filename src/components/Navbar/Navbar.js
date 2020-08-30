1af1542f153d0dcd26f84f971ec3d0a723f0cae3
Blessedadudu <adududuke@gmail.com>
2020-08-30 14:24:37 +0100

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,12 @@ import './Navbar.scss'
 function Nabar() {
     return (
         <div className='Navbar flex-2'>
+            <div className='toggle-nav'>
+                <h1>hello</h1>
+            </div>
             <div className='flex-2 flex--2'>
                 <img src='https://a.slack-edge.com/4a5c4/marketing/img/icons/icon_slack.svg' alt='slack icon'/>
-                <ul className='flex-2'>
+                <ul className='flex-2 desktop-nav'>
                     <li className='li--1'>Why Slack?</li>
                     <div className='why-hover'>
                         <ul>
@@ -16,20 +19,36 @@ function Nabar() {
                             <li>Customers</li>
                         </ul>
                     </div>
-                    <li>Solutions</li>
+                    <li className='li--2'>Solutions</li>
+                    <ul className='solutions'>
+                        <li>Remote Work</li>
+                        <li>Distance Learning</li>
+                        <li>Engineering</li>
+                        <li>Sales</li>
+                        <li>IT</li>
+                        <li>Marketing</li>
+                        <li>Customer Support</li>
+                        <li>Human Resources</li>
+                        <li>Project Management</li>
+                        <li>Media</li>
+                    </ul>
                     <li>Resources</li>
                     <li>enterprises</li>
                     <li>Pricing</li>
                     <li></li>
                 </ul>
             </div>
-            <div className='flex-2 contact'>
+            <div className='flex-2 contact desktop-nav'>
                 <ul className='flex-2'>
                     <li>Contact sales</li>
                     <li>Signin</li>
                 </ul>
                 <button>Try for free</button>
             </div>
+            <div className='nav-burger'>
+                <span className="iconz">&nbsp;</span>
+            </div>
+           
         </div>
     )
 }
