f4d1f20d95ab42ea7c53e1f46f07fbb8caf40e94
Blessedadudu <adududuke@gmail.com>
2020-09-02 10:02:11 +0100

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,14 @@ import { FaTwitter } from "react-icons/fa";
 import { GrFacebook } from "react-icons/gr";
 import { ImYoutube } from "react-icons/im";
 import { ImLinkedin} from "react-icons/im";
+import { AiOutlineCopyright } from "react-icons/ai";
+
+
 
 function Footer() {
     return (
         <div className='footer'>
-            <div  className='flex'>
+            <div  className='flex flex-svg'>
                 <div className='svg'>
                     <svg width="54" height="54" class="c-nav--footer__svgicon c-slackhash" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M19.712.133a5.381 5.381 0 0 0-5.376 5.387 5.381 5.381 0 0 0 5.376 5.386h5.376V5.52A5.381 5.381 0 0 0 19.712.133m0 14.365H5.376A5.381 5.381 0 0 0 0 19.884a5.381 5.381 0 0 0 5.376 5.387h14.336a5.381 5.381 0 0 0 5.376-5.387 5.381 5.381 0 0 0-5.376-5.386" fill="#36C5F0"></path><path d="M53.76 19.884a5.381 5.381 0 0 0-5.376-5.386 5.381 5.381 0 0 0-5.376 5.386v5.387h5.376a5.381 5.381 0 0 0 5.376-5.387m-14.336 0V5.52A5.381 5.381 0 0 0 34.048.133a5.381 5.381 0 0 0-5.376 5.387v14.364a5.381 5.381 0 0 0 5.376 5.387 5.381 5.381 0 0 0 5.376-5.387" fill="#2EB67D"></path><path d="M34.048 54a5.381 5.381 0 0 0 5.376-5.387 5.381 5.381 0 0 0-5.376-5.386h-5.376v5.386A5.381 5.381 0 0 0 34.048 54m0-14.365h14.336a5.381 5.381 0 0 0 5.376-5.386 5.381 5.381 0 0 0-5.376-5.387H34.048a5.381 5.381 0 0 0-5.376 5.387 5.381 5.381 0 0 0 5.376 5.386" fill="#ECB22E"></path><path d="M0 34.249a5.381 5.381 0 0 0 5.376 5.386 5.381 5.381 0 0 0 5.376-5.386v-5.387H5.376A5.381 5.381 0 0 0 0 34.25m14.336-.001v14.364A5.381 5.381 0 0 0 19.712 54a5.381 5.381 0 0 0 5.376-5.387V34.25a5.381 5.381 0 0 0-5.376-5.387 5.381 5.381 0 0 0-5.376 5.387" fill="#E01E5A"></path></g></svg> 
                 </div>
@@ -76,25 +79,25 @@ function Footer() {
                 </div>
             </div>
            
-            <div>
-                <div>
-                   <p>Status</p>
-                    <p>Privacy & TermsContact</p>
+            <div className='flex flex-status'>
+                <div className='flex status'>
+                    <p>Status</p>
+                    <p>Privacy & Terms</p>
                     <p>Conatct US</p>
-                    <div>
+                    <div className='flex icon'>
                         <FiGlobe/>
                         <p>Change Region</p>
-                        <BsChevronDown/>
+                        <BsChevronDown className='arrow'/>
                     </div> 
                 </div>
-                <div>
-                    <FaTwitter/>
-                    <GrFacebook/>
-                    <ImYoutube/>
-                    <ImLinkedin/>
+                <div className='social-links'>
+                    <FaTwitter className='icons'/>
+                    <GrFacebook className='icons'/>
+                    <ImYoutube className='icons'/>
+                    <ImLinkedin className='icons'/>
                 </div>
-                
             </div>
+            <p className='copyright'> &#169; Copyright {(new Date().getFullYear())} Slack Technologies, Inc. All rights reserved. Various trademarks held by their respective owners.</p>
         </div>
     )
 }
